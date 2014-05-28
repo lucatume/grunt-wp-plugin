80731075af22210b8b3782ee8a3d6f196aef3ca1
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-05-28 17:53:45 +0200

    modified the template to use the src folder in place of the includes one

--- template.js
+++ template.js
@@ -5,21 +5,15 @@
  * Copyright (c) 2013 Eric Mann, 10up
  * Licensed under the MIT License
  */
-
 'use strict';
-
 // Basic template description
 exports.description = 'Create a WordPress plugin.';
-
 // Template-specific notes to be displayed before question prompts.
 exports.notes = '';
-
 // Template-specific notes to be displayed after the question prompts.
 exports.after = '';
-
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
-
 // The actual init template
 exports.template = function(grunt, init, done) {
     init.process({}, [
@@ -55,7 +49,6 @@ exports.template = function(grunt, init, done) {
             'grunt-contrib-sass': "~0.7.x",
             'grunt-autoprefixer': '0.7.x'
         };
-
         // Sanitize names where we need to for PHP/JS
         props.name = props.title.replace(/\s+/g, '-').toLowerCase();
         // Development prefix (i.e. to prefix PHP function names, variables)
@@ -67,15 +60,12 @@ exports.template = function(grunt, init, done) {
         // An additional value that won't conflict with NodeUnit unit tests.
         props.js_test_safe_name = props.js_safe_name === 'test' ? 'myTest' : props.js_safe_name;
         props.js_safe_name_caps = props.js_safe_name.toUpperCase();
-
         // Files to copy and process
         var files = init.filesToCopy(props);
-
         switch (props.css_type.toLowerCase()[0]) {
             case 'l':
                 delete files['assets/css/sass/' + props.js_safe_name + '.scss'];
                 delete files['assets/css/src/' + props.js_safe_name + '.css'];
-
                 props.devDependencies["grunt-contrib-less"] = "~0.5.0";
                 props.css_type = 'less';
                 break;
@@ -83,40 +73,34 @@ exports.template = function(grunt, init, done) {
             case undefined:
                 delete files['assets/css/less/' + props.js_safe_name + '.less'];
                 delete files['assets/css/sass/' + props.js_safe_name + '.scss'];
-
                 props.css_type = 'none';
                 break;
                 // SASS is the default
             default:
                 delete files['assets/css/less/' + props.js_safe_name + '.less'];
                 delete files['assets/css/src/' + props.js_safe_name + '.css'];
-
                 props.devDependencies["grunt-contrib-sass"] = "~0.2.2";
                 props.css_type = 'sass';
                 break;
         }
-
         console.log(files);
-
-        // rename the includes/plugin folder to includes/prefix
+        // rename the src/plugin folder to src/prefix
         // hint from PeteAUK at:
         // http://stackoverflow.com/questions/11852283/rename-templates-folders-with-a-gruntjs-custom-init-task
         var files = init.filesToCopy(props),
-            folder_name = 'includes/' + props.prefix;
+            folder_name = 'src/' + props.namespace + '/' + props.prefix;
         for (var file in files) {
-            if (file.indexOf('includes/plugin/') > -1) {
+            if (file.indexOf('src/plugin/') > -1) {
                 var path = files[file],
-                    newFile = file.replace('includes/plugin/', folder_name + '/');
+                    newFile = file.replace('src/plugin/', folder_name + '/');
                 files[newFile] = path;
                 delete files[file];
             }
         }
         // Actually copy and process files
         init.copyAndProcess(files, props);
-
         // Generate package.json file
         init.writePackageJSON('package.json', props);
-
         // Done!
         done();
     });
